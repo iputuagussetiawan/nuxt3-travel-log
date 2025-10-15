3217883fcb213c069e3e1857a087d77ab6154694
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-10-15 09:06:34 +0800

    better auth integration

---
+++ components/ui/auth-button.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import Button from './button/Button.vue'
+import { createAuthClient } from "better-auth/client"
+const authClient =  createAuthClient()
+async function signIn(){
+    await authClient.signIn.social({
+        provider: "github",
+        callbackUrl: "/dashboard",
+    })
+}
+</script>
+<template>
+    <div>
+        <Button @click="signIn">
+            Continue with GitHub
+            <lucide-github />
+        </Button>
+    </div>
+</template>
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
+<script lang="ts" setup>
+import { authClient } from '@/lib/auth-client'
+import AuthButton from '../components/ui/auth-button.vue'
+const session = authClient.useSession();
+</script>
 <template>
     <div>
         <h1>Welcome to the homepage</h1>
         <Button>Welcome</Button>
+        <AuthButton />
+
+        <pre>{{ session.data }}</pre>
     </div>
 </template>
